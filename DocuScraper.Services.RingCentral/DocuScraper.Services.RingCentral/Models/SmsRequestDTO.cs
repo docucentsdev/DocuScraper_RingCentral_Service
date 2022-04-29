3a08f27d0abbb1a31e801df388e764c1d914a98c
Sreejith Gopinathan <jithappan@gmail.com>
2022-04-29 22:06:19 +0530

    Initial Commit
    Initial Commit

---
+++ DocuScraper.Services.RingCentral/DocuScraper.Services.RingCentral/Models/SmsRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocuScraper.Services.RingCentral.Models
+{
+    public class SmsRequestDTO
+    {
+        [Required(ErrorMessage = "Recipient number is required.")]
+        [RegularExpression("^[0-9]{10}$", ErrorMessage = "Phone should be a 10 digit valida phone number.")]
+        public string RecipientNumber { get; set; }
+
+        [Required(ErrorMessage = "Text message is required.")]
+        public string Message { get; set; }
+    }
+}
